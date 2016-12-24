6299ea4b554087e8f1979f4d3657fa52a2a88416
Bret Gourdie <BretGourdie@gmail.com>
2016-12-24 01:15:30 -0500

    Added prompt for team method

--- weakness-subset.py
+++ weakness-subset.py
@@ -1,1 +1,18 @@
-print("****Weakness Subset Calculator****")
+def intro():
+    print("****Weakness Subset Calculator****")
+
+def promptForTeam():
+    team = []
+    print("Please enter the Pokemon to analyze, one at a time.\n"
+            + "When finished, hit Enter with nothing typed.")
+
+    curPoke = "blank"
+    while curPoke != "":
+        curPoke = input()
+        if curPoke != "":
+            team.append(curPoke)
+
+    return team
+
+intro()
+team = promptForTeam()
